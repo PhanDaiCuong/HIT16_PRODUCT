70ea14129e3ab85812baac59d76c1e8738244b05
itzzchann <tutrananh444@gmail.com>
2026-02-10 23:04:27 +0700

    add app_streamlit from Chau

---
+++ src/schemas/__init__.py
@@ -0,0 +1,22 @@
+
+from .parking_model import (
+    ImageDetectionRequest,
+    DetectedObject,
+    ParkingSpot,
+    DetectionSummary,
+    DetectionResponse,
+    PolygonConfig,
+    DetectionConfig,
+)
+
+__all__ = [
+    "ImageDetectionRequest",
+    "DetectedObject",
+    "ParkingSpot",
+    "DetectionSummary",
+    "DetectionResponse",
+    "PolygonConfig",
+    "DetectionConfig",
+]
+
+
